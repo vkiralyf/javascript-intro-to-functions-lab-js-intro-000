d6b92b5cff0ccd23ac29cd0215924cc93d36ccd9
Victor K <vkiralyf@gmail.com>
2018-08-10 14:11:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ function sayHiToGrandma(string) {
   if (string===string.toUpperCase()) {
       return "YES INDEED!"
 
+  }else if (string===string.toLowerCase()) {
+      return"I can\'t hear you!"
   }else if (string==="I love you Grandma.") {
       return "I love you, too."
   }
