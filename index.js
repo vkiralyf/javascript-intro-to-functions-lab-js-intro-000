8214309d5c6217d595546ce27d5134457d8304cf
Victor K <vkiralyf@gmail.com>
2018-08-10 14:39:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ function whisper(string) {
 
 }
 function logShout(string) {
-  lowercase=string.toLowerCase()
-  console.log(lowercase)
+  console.log(string.toUpperCase())
 
 }
 function logWhisper(string) {
+  console.log(string.toLowerCase())
 
 }
 function sayHiToGrandma(string) {
