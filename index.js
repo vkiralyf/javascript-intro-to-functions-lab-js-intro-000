0f153777a5501e7edd9695c12a22ff655c705976
Victor K <vkiralyf@gmail.com>
2018-08-10 14:08:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ function whisper(string) {
 
 }
 function logShout(string) {
-  string.toUpperCase()
-  console.log(string);
+  console.log(string.toLowerCase());
 
 }
 function sayHiToGrandma(string) {
