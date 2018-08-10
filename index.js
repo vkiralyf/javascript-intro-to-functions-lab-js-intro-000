c29dd3746203c43f966995edd1bbb7c2e0fe0dae
Victor K <vkiralyf@gmail.com>
2018-08-10 13:54:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ function logShout(string) {
 }
 function sayHiToGrandma(string) {
   if (string===string.toUpperCase) {
+      return "YES INDEED!"
 
+  }else if (string==="I love you Grandma.") {
+      return "I love you, too."
   }
 
 }
